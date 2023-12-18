12aac4d445eca33650591987e56e360de3d2d990
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-12-18 01:04:47 +0000

    java学習

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 // public class Main {
 //   public static void main(String[] args) {
 //     MyClass myClass = new MyClass();      //MyClassインスタンスの作成
-    
+
 //     myClass.hello();       //MyClassインスタンスのhelloメソッドにアクセス
 //   }
 // }
@@ -12,7 +12,7 @@
 //     MyClass myClass = new MyClass();
 //     MyClass iroha = new MyClass();
 //     MyClass sensei = new MyClass();     //各インスタンスの作成
-    
+
 //     myClass.hello();
 //     iroha.hello();
 //     sensei.hello();      //各インスタンスのhelloメソッドにアクセス
@@ -20,17 +20,46 @@
 // }
 
 
-public class Main {      //最初に実行するクラス
+// public class Main {      //最初に実行するクラス
+//   public static void main(String[] args) {
+//     MyClass myClass = new MyClass();
+//     MyClass iroha = new MyClass();
+//     MyClass sensei = new MyClass();     //各インスタンスの作成
+
+//     iroha.myName = "いろは";
+//     sensei.myName = "センセイ";     //各インスタンスのmyNameフィールドに値を
+
+//     myClass.hello();
+//     iroha.hello();
+//     sensei.hello();      //各インスタンスのhelloメソッドにアクセス
+//   }
+// }
+
+
+// public class Main {      //最初に実行するクラス
+//   public static void main(String[] args) {
+//     CalcQuiz q = new CalcQuiz();       //クイズを出すインスタンス
+
+//     System.out.println(q.getQuestion());       //問題を出す
+//     System.out.println(q.getAnswer());        //答えをいう
+//   }
+// }
+
+
+public class Main {
   public static void main(String[] args) {
-    MyClass myClass = new MyClass();
-    MyClass iroha = new MyClass();
-    MyClass sensei = new MyClass();     //各インスタンスの作成
-    
-    iroha.myName = "いろは";
-    sensei.myName = "センセイ";     //各インスタンスのmyNameフィールドに値を
-    
-    myClass.hello();
-    iroha.hello();
-    sensei.hello();      //各インスタンスのhelloメソッドにアクセス
+    int quizNum = 5;      //問題数
+    CalcQuiz [] quiz = new CalcQuiz[quizNum];      //問題を作るインスタンスを入れる配列
+
+    for (int i = 0; i < quizNum; i++) {       //問題を作るインスタンスを作る
+      quiz[i] = new CalcQuiz();
+    }
+    for (int i = 0; i < quizNum; i++) {
+      System.out.println("問" + i + ":" + quiz[i].getQuestion());      //すべての問題を表示する
+    }
+    System.out.println("--------------");
+    for (int i = 0; i < quizNum; i++) {
+      System.out.println("答" + i + ":" + quiz[i].getAnswer());
+    }
   }
 }
